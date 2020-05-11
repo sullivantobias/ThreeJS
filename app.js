d605dc2a7d14940be6745cf6e6e0ed37dd20483c
sullivan <sullytobias@gmail.com>
2020-05-11 19:51:45 +0200

    Init Release 1.0.0

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require("express"),
-  path = require("path"),
-  app = express();
+    path = require("path"),
+    app = express();
 
 // Express Middleware for serving static files
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  res.redirect("/examples/solarSystem.html");
+    res.redirect("/examples/solarSystem.html");
 })
 
 app.listen(8080);
